546ea837c60e4f684c2bf34ac67aef5024b30b3d
Jonathan Watson <jonathanw12@outlook.com>
2020-06-06 15:03:14 +0100

    Change folder structure

--- kivy_games/__main__.py
+++ kivy_games/__main__.py
@@ -1,12 +1,12 @@
-from kivy.uix.label import Label
 from kivy.app import App
 import kivy
+from kivy_games.main_menu import MainMenu
 kivy.require('1.11.1')
 
 
 class Main(App):
     def build(self):
-        return Label(text='Testing')
+        return MainMenu()
 
 
 if __name__ == '__main__':
