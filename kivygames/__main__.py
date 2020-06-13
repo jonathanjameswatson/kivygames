cdd57497e36e4cb1cf44a8c180ef2ff5e698772f
Jonathan Watson <jonathanw12@outlook.com>
2020-06-13 17:41:25 +0100

    Use async to run kivy

--- kivygames/__main__.py
+++ kivygames/__main__.py
@@ -1,3 +1,4 @@
+import asyncio
 from os import scandir
 from os.path import abspath, join
 from importlib import import_module
@@ -26,8 +27,18 @@ class KivyGamesApp(App):
         Window.clearcolor = (1, 1, 1, 1)
         self.title = 'Kivy Games'
 
+    def app_func(self):
+        async def run_wrapper():
+            await self.async_run()
+
+        return asyncio.gather(run_wrapper())
+
 
 if __name__ == '__main__':
     importKv()
     resource_add_path(abspath(f'{__file__}/../assets'))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(KivyGamesApp().app_func())
+    loop.close()
     KivyGamesApp().run()
