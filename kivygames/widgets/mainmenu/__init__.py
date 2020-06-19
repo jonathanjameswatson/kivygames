0ab113154a91020ada73f71264c16b96385eada3
Jonathan Watson <jonathanw12@outlook.com>
2020-06-19 14:07:05 +0100

    Made main menu automatically add games

--- kivygames/widgets/mainmenu/__init__.py
+++ kivygames/widgets/mainmenu/__init__.py
@@ -1,6 +1,19 @@
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.app import App
 
 
-class MainMenu(Widget):
+class MainMenu(BoxLayout):
     def __init__(self, **kwargs):
-        Widget.__init__(self, **kwargs)
+        BoxLayout.__init__(self, **kwargs)
+        buttonGrid = self.ids["buttonGrid"]
+        self.app = App.get_running_app()
+        for i, gameLayout in enumerate(self.app.gameLayouts):
+            button = Button(
+                text=gameLayout.name,
+                on_press=lambda sender, i=i: self.setGame(gameLayout),
+            )
+            buttonGrid.add_widget(button)
+
+    def setGame(self, gameLayout):
+        self.app.root.current = gameLayout.__name__.lower()
